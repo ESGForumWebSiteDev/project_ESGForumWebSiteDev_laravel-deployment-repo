330913905b8ec38d6b84557fce98edc39acf610a
devYuMinKim <gimyumin40@gmail.com>
2023-07-15 15:13:42 +0900

    ✨ feat : Implement login logic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Requests\LoginRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('/login', [LoginController::class, 'login'])->name('api.login');
+Route::post('/register', [RegisterController::class, 'register'])->name('api.register');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
