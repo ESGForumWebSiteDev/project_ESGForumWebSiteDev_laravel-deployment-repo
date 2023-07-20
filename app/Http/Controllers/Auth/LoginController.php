0780177b188dc93ff9569c4c43f3a1068317969a
devYuMinKim <gimyumin40@gmail.com>
2023-07-20 16:56:12 +0900

    🎨 refactor : Segment controllers by function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,31 +2,35 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+  /**
+   * 로그인 처리 메소드
+   */
   public function login(LoginRequest $request)
   {
-    $credentials = $request->only('userId', 'password');
+    $credentials = $request->only('email', 'password');
 
-    if (Auth::attempt($credentials)) {
+    if (Auth::guard('web')->attempt($credentials)) {
+      $user = User::firstWhere('email', $request->email);
+      $tokenExpirationDays = config('auth.personal_access_tokens.expire_after_days');
+      $token = $user->createToken('api-token', [], now()->addDays($tokenExpirationDays))->plainTextToken;
+
       return response()->json([
-        'message' => 'Login successful!'
+        'success' => true,
+        'message' => '로그인 성공!',
+        'token' => $token
       ], 200);
-    }
+      }
 
-    return response()->json([
-      'error' => 'User ID and/or password is incorrect.'
-    ], 401);
-  }
-
-  public function logout(Request $request)
-  {
-    Auth::logout();
-    return;
+      return response()->json([
+        'success' => false,
+        'error' => '이메일 또는 비밀번호가 올바르지 않습니다.'
+      ], 401);
   }
 }
