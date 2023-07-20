a80db1bfbe46642b2a0944e2f2db7770456a1b99
devYuMinKim <gimyumin40@gmail.com>
2023-07-20 16:55:36 +0900

    ✨ feat : Implement check api

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Auth\CheckController;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::post('/login', [LoginController::class, 'login'])->name('api.login');
-Route::post('/register', [RegisterController::class, 'register'])->name('api.register');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * 로그인 관련 API
+ */
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [LoginController::class, 'login'])->name('api.login');
+    Route::post('/register', [RegisterController::class, 'store'])->name('api.register');
+    Route::post('/logout', [LogoutController::class, 'logout'])->middleware(['auth:sanctum'])->name('api.logout');
+    Route::post('/check', [CheckController::class, 'isLoggedIn'])->middleware(['auth:sanctum'])->name('api.check');
 });
