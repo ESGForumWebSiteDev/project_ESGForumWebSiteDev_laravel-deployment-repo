fffd0220c9c65fef9df9b2201d5d551ab75b749f
devYuMinKim <gimyumin40@gmail.com>
2023-07-22 01:42:56 +0900

    ✨ feat : Implement Business CRUD API

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -7,10 +7,41 @@ use Illuminate\Http\Request;
 
 class BusinessController extends Controller
 {
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kind' => 'required|string|max:40',
+            'content' => 'required|string',
+        ]);
+
+        $business = Business::create($request->all());
+
+        return response()->json($business, 201);
+    }
+
     public function index()
     {
         $business = Business::all();
 
         return response()->json($business);
+    }
+
+    public function update(Request $request, Business $business)
+    {
+        $request->validate([
+            'kind' => 'required|string|max:40',
+            'content' => 'required|string',
+        ]);
+
+        $business->update($request->all());
+
+        return response()->json($business);
+    }
+
+    public function destroy(Business $business)
+    {
+        $business->delete();
+
+        return response()->json(['message' => 'Business deleted successfully']);
     }
 }
