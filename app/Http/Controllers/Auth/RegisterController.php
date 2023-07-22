6564ca72b76d0f4fa18bb244c82754f5a901df77
devYuMinKim <gimyumin40@gmail.com>
2023-07-22 23:21:07 +0900

    ✨ feat : Implement Login logic with JWT

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -14,15 +14,19 @@ class RegisterController extends Controller
    */
   public function store(RegisterRequest $request)
   {
-    User::create([
+    $user = User::create([
       'email' => $request->email,
       'password' => Hash::make($request->password),
       'name' => $request->name,
       'affiliation' => $request->affiliation,
     ]);
 
+    $token = $user->createToken('auth_token')->plainTextToken;
+
     return response()->json([
-        'message' => 'Registration successful! You can now log in.'
+      'access_token' => $token,
+      'message' => 'Registration successful! You can now log in.',
+      'token_type' => 'Bearer',
     ], 201);
   }
 }
