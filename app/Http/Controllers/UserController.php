21bb83d2320ba615a94bf53b8c851c9c4142eb79
Z00One <9000248@g.yju.ac.kr>
2023-09-03 18:25:54 +0900

    fix(UserController): Fix validation rule in approval method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,14 +21,19 @@ class UserController extends Controller
     public function approval(Request $request)
     {
         $request->validate([
-            'name' => 'required|string',
-            'affiliation' => 'required|string'
+            'email' => 'required|string',
         ]);
 
-        User::where('name', $request->input('name'))
-            ->where('affiliation', $request->input('affiliation'))
+        User::find($request->input('email'))
             ->update(['authority' => 0]);
 
         return response()->json('User approved successfully', 201);
+    }
+
+    public function destroy($email)
+    {
+        User::find($email)->delete();
+
+        return response()->json('User deleted successfully', 204);
     }
 }
