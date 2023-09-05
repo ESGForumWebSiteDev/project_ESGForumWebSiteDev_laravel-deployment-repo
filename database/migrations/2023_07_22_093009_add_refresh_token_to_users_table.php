121ce0ebbb4e6994effe4c084d02cfd59e10cf36
devYuMinKim <gimyumin40@gmail.com>
2023-09-05 11:48:30 +0900

    refactor(migration): modify wrong letter

--- database/migrations/2023_07_22_093009_add_refresh_token_to_users_table.php
+++ database/migrations/2023_07_22_093009_add_refresh_token_to_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('refresh_token')->nullable()->afrer('authority');
+            $table->text('refresh_token')->nullable()->after('authority');
         });
     }
 
