fa224d4528ab61e2f02e0c8a8199bd6174eb3d42
Z00One <9000248@g.yju.ac.kr>
2023-09-05 21:41:12 +0900

    feat: Update members table migration for email and authentication fields

---
+++ database/migrations/2023_09_05_122626_add_columns_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('email', 20)->unique();
+            $table->string('password', 255);
+            $table->integer('authority')->nullable()->comment('null - 승인X, 0 - 사용자, 1 - 관리자');
+            $table->text('refresh_token')->nullable()->afrer('authority');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('password');
+            $table->dropColumn('authority');
+            $table->dropColumn('refresh_token');
+        });
+    }
+};
