26eb65f0e9e238dcd3d40139fa1bcf7e02077a46
Z00One <9000248@g.yju.ac.kr>
2023-09-09 19:20:12 +0900

    feat(member): Add rejection functionality

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -9,7 +9,7 @@ class MemberController extends Controller
 {
     public function count()
     {
-        $members = Member::all()->count();
+        $members = Member::where('authority', '<>', env('REJECTED_MEMBER'))->count();
         $applicants = Member::where('authority', null)->count();
 
         return response()->json([
@@ -20,7 +20,7 @@ class MemberController extends Controller
 
     public function index()
     {
-        $members = Member::all();
+        $members = Member::where('authority', '<>', env('REJECTED_MEMBER'))->get();
 
         return response()->json($members);
     }
@@ -32,6 +32,15 @@ class MemberController extends Controller
             'affiliation' => 'required|string'
         ]);
 
+        $isMember = Member::where('name', $request->input('name'))
+            ->where('affiliation', $request->input('affiliation'))
+            ->where('authority', '<>', env('REJECTED_MEMBER'))
+            ->get();
+
+        if ($isMember) {
+            return response()->json('conflict with existing members', 409);
+        }
+
         $member = Member::create([
             'name' => $request->input('name'),
             'affiliation' => $request->input('affiliation'),
@@ -40,11 +49,15 @@ class MemberController extends Controller
         return response()->json($member, 201);
     }
 
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        Member::where('id', $id)->delete();
+        $request->validate([
+            'ids' => 'required|array',
+            'ids.*' => 'required|integer',
+        ]);
 
-        return response()->json(['message' => 'Committee deleted successfully'], 204);
+        Member::whereIn('id', $request->input('ids'))->delete();
+        return response()->json(['message' => 'Member deleted successfully'], 204);
     }
 
     public function update(Request $request)
@@ -64,13 +77,26 @@ class MemberController extends Controller
     public function approval(Request $request)
     {
         $request->validate([
-            'id' => 'required|integer',
+            'ids' => 'required|array',
+            'ids.*' => 'required|integer',
         ]);
 
-        Member::find($request->input('id'))
+        Member::whereIn('id', $request->input('ids'))
             ->update(['authority' => 0]);
 
-        return response()->json('User approved successfully', 201);
+        return response()->json('Member approved successfully', 201);
+    }
+    public function rejection(Request $request)
+    {
+        $request->validate([
+            'ids' => 'required|array',
+            'ids.*' => 'required|integer',
+        ]);
+
+        Member::whereIn('id', $request->input('ids'))
+            ->update(['authority' => env('REJECTED_MEMBER')]);
+
+        return response()->json('Member rejected successfully', 201);
     }
 
     public function applicants()
